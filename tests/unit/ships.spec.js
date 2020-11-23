3eb51e58d83d35a6466b6ae4863cf8d28e584384
ellielle <40385743+ellielle@users.noreply.github.com>
2020-11-23 05:31:47 -0500

    init

--- tests/unit/ships.spec.js
+++ tests/unit/ships.spec.js
@@ -4,8 +4,7 @@ import Gameboard from "@/components/Gameboard.vue"
 describe("ship functionality", () => {
   it("takes damage when hit", () => {
     const wrapper = mount(Gameboard);
-    const hitShip = wrapper.get('["data-test="hit-ship"]');
 
-    expect(hitShip).toContain("X");
+    expect(wrapper.find("O").element).not.to
   });
 });
